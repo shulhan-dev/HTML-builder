80dd38172c2eee86e8d9397db73cf191d7201f35
Shulhan Andrei <shulhan-dev@gmail.com>
2025-01-20 11:10:09 +0300

    fix: change message

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -9,13 +9,13 @@ process.stdout.write('Hello, write some a text... \n');
 
 process.stdin.on('data', (data) => {
   if (data.toString().trim() == 'exit') {
-    process.stdout.write('----- Прощай ------ \n');
+    process.stdout.write('----- Bye ------ \n');
     process.exit();
   }
   writeableStream.write(data);
 });
 
 process.on('SIGINT', () => {
-  process.stdout.write('----- Прощай ------ \n');
+  process.stdout.write('----- Bye ------ \n');
   process.exit();
 });

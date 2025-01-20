eeb1b4d499999e9960836bdd592150e7565915fe
Shulhan Andrei <shulhan-dev@gmail.com>
2025-01-20 11:40:03 +0300

    fix: change args

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,13 +1,10 @@
 const path = require('path');
 const fs = require('fs/promises');
-
-const copyDir = async (srcFolder, distFolder) => {
-  const files = await fs.readdir(srcFolder, {
-    withFileTypes: true,
-  });
-
+const copyDir = async (nameSrc, nameDist) => {
+  const srcFolder = path.join(__dirname, `${nameSrc}`);
+  const distFolder = path.join(__dirname, `${nameDist}`);
+  const files = await fs.readdir(srcFolder, { withFileTypes: true });
   await fs.mkdir(distFolder, { recursive: true });
-
   for (let file of files) {
     await fs.copyFile(
       path.join(srcFolder, `${file.name}`),
@@ -15,5 +12,4 @@ const copyDir = async (srcFolder, distFolder) => {
     );
   }
 };
-
-copyDir(path.join(__dirname, 'files'), path.join(__dirname, 'files-copy'));
+copyDir('files', 'files-copy');

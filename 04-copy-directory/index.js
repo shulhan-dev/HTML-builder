79b2a2d0a7cafaf1aa9cd864686061bb2b5c8db8
Shulhan Andrei <shulhan-dev@gmail.com>
2025-01-20 11:24:57 +0300

    feat: completed task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs/promises');
+
+const copyDir = async (srcFolder, distFolder) => {
+  const files = await fs.readdir(srcFolder, {
+    withFileTypes: true,
+  });
+
+  await fs.mkdir(distFolder, { recursive: true });
+
+  for (let file of files) {
+    await fs.copyFile(
+      path.join(srcFolder, `${file.name}`),
+      path.join(distFolder, `${file.name}`),
+    );
+  }
+};
+
+copyDir(path.join(__dirname, 'files'), path.join(__dirname, 'files-copy'));

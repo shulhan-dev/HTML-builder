b01a32c384f0cef9a8a990cb488a734e1314451f
Shulhan Andrei <shulhan-dev@gmail.com>
2025-01-23 13:42:04 +0300

    fix: delete files from copy folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,8 +3,15 @@ const fs = require('fs/promises');
 const copyDir = async (nameSrc, nameDist) => {
   const srcFolder = path.join(__dirname, `${nameSrc}`);
   const distFolder = path.join(__dirname, `${nameDist}`);
-  const files = await fs.readdir(srcFolder, { withFileTypes: true });
+
   await fs.mkdir(distFolder, { recursive: true });
+  const distFiles = await fs.readdir(distFolder);
+  for (const file of distFiles) {
+    await fs.unlink(path.join(distFolder, file));
+  }
+
+  const files = await fs.readdir(srcFolder, { withFileTypes: true });
+
   for (let file of files) {
     await fs.copyFile(
       path.join(srcFolder, `${file.name}`),
